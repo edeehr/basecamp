f20319d88eefabc03af635a264f0400a50a8ac6e
Kevin Dillon <kdillon@2ndwatch.com>
2018-02-09 21:57:02 -0500

    added event_description to output.tf

--- 2-best_practices/outputs.tf
+++ 2-best_practices/outputs.tf
@@ -1,6 +1,11 @@
 #####
 # Specify Outputs - this is a simple example, could combine with main.tf file
 #####
+
+output "event_description" {
+  value = "${var.event_description}"
+}
+
 output "elb_address" {
   value = "${aws_elb.web-elb.dns_name}"
 }
@@ -8,4 +13,3 @@ output "elb_address" {
 output "web_server_address" {
   value = "${aws_instance.web_server.*.public_ip}"
 }
-
